415ca8065d234aab7612282f87620e8dd812680d
Muhammad Ali Riaz <muhammadaliriaz6055@gmail.com>
2025-01-30 19:23:18 +0500

    initial commit

--- task1.py
+++ task1.py
@@ -2,10 +2,11 @@ def min_max_normalisation (my_list):
     x_min=min(my_list)
     x_max=max(my_list)
     for i in my_list:
-        x_scaled=(i-x_min)/(x_max-x_min)
-    return [x_scaled for i in my_list]
+        scaled=(i-x_min)/(x_max-x_min)
+        x_scaled.append(round(scaled,3))
+    return x_scaled
 
 mylist=list(map(int,input("Enter number separated by space").split()))
-
-x_scaled = min_max_normalisation(mylist)
+x_scaled=[]
+min_max_normalisation(mylist)
 print(x_scaled)
