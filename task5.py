9420468beaff96dc3127de6503c61d975069e5ae
Muhammad Ali Riaz <muhammadaliriaz6055@gmail.com>
2025-01-30 19:56:10 +0500

    commit 2

--- task5.py
+++ task5.py
@@ -6,8 +6,6 @@ c=int(input("Enter C: ",))
 r_col=r//8
 c_col=c//8
 a = numpy.ones((r,c,3),dtype=numpy.uint8)*255
-cv2.imshow('win',a)
-cv2.waitKey()
 
 a[0:r_col,0:c_col,:]=[0,0,0] #black
 a[0:r_col,c-c_col:c,:]=[255,0,0] #blue
