66aceb0f11ffcd53d4616b62bd1c57f99e2581f0
Muhammad Ali Riaz <muhammadaliriaz6055@gmail.com>
2025-01-30 19:55:29 +0500

    commit

--- task5.py
+++ task5.py
@@ -1,18 +1,18 @@
 import numpy
 import cv2
 
-r=int(input("Enter R",))
-c=int(input("Enter C",))
+r=int(input("Enter R: ",))
+c=int(input("Enter C: ",))
+r_col=r//8
+c_col=c//8
 a = numpy.ones((r,c,3),dtype=numpy.uint8)*255
 cv2.imshow('win',a)
 cv2.waitKey()
 
-a[0:50,0:50,:]=[0,0,255]
-
-a[0:50,c-50:c,:]=[0,255,0]
-
-a[r-50:r,c-50:c,:]=[255,255,0]
-a[0:50,c:c-50,:]=[255,255,0]
+a[0:r_col,0:c_col,:]=[0,0,0] #black
+a[0:r_col,c-c_col:c,:]=[255,0,0] #blue
+a[r-r_col:r,c-c_col:c,:]=[0,255,0] #green
+a[r-r_col:r,0:c_col,:]=[0,0,255] #red
 
 cv2.imshow('win',a)
 cv2.waitKey()
